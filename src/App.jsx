e0c3267c4e8399f5438ca41fbe3f4afeb63324ef
sevincresulova <114071630+sevincresulova@users.noreply.github.com>
2022-11-22 14:36:00 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './components/Header/index.css'
+
+const App=() => {
+    return (
+        <>
+        <div className='general'>
+        <h3>Sevinj Rasulova</h3>
+        <p className='first'> «Now I can create component»</p>
+        </div>
+        </>
+    )
+}
+ export default App
